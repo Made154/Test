69b7bb263d82460a9cbcda088502e0d7aaa4daca
made154 <pernotmahe33@gmail.com>
2025-10-06 10:25:15 +0200

    sauvegarde

--- permier.py
+++ permier.py
@@ -1,10 +1,6 @@
-n = int(input("Enter un nombre: "))
-premier = True
-for diviseur in range(2, n):
-    reste  = n % diviseur
-    if reste == 0:
-        print(n, "ce n'est pas un monbre premier", diviseur)
-        premier = False
-
-if premier:
-    print(n, "c'est un nombre premier")
+from utils import estPrmier
+n = int(input("Entrez un nombre entier : "))
+if estPrmier(n):
+    print(n, "est un nombre premier")
+else:
+    print(n, "n'est pas un nombre premier")
